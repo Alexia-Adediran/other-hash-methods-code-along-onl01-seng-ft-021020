fe247d5202ab928d3ad31b7a70795d7bc8770ef8
Alexia Adediran <alexiaadediran30@gmail.com>
2020-02-21 14:31:20 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -8,9 +8,12 @@ require 'pry'
 #  grains: ["rice", "pasta"]
 # }
 
+# def get_the_keys(groceries)
+#   groceries.keys
+# end
+
+# get_the_keys(groceries)
+# end
 def get_the_keys(groceries)
   groceries.keys
-end
-
-get_the_keys(groceries)
 end
